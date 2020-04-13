623cc02b106162c6be93c9653090231d7d0f7085
Chris <christoph.kralj@gmail.com>
2020-04-13 15:04:57 +0200

    Fixed sorting with one element arrays

--- src/resources/converters/custom-sort.ts
+++ src/resources/converters/custom-sort.ts
@@ -1,13 +1,19 @@
 export class CustomSortValueConverter {
   toView(array, config) {
     let factor = (config.direction || "ascending") === "ascending" ? 1 : -1;
-    return array.sort((a, b) => {
-      if (isNaN(a[config.propertyName]) || isNaN(b[config.propertyName])) {
-        return a[config.propertyName].localeCompare(b[config.propertyName]) * factor;
-      }
-      else {
-        return (a[config.propertyName] - b[config.propertyName]) * factor;
-      }
-    });
+    console.log(array)
+    if (array.length > 0) {
+      return array.sort((a, b) => {
+        if (isNaN(a[config.propertyName]) || isNaN(b[config.propertyName])) {
+          return a[config.propertyName].localeCompare(b[config.propertyName]) * factor;
+        }
+        else {
+          return (a[config.propertyName] - b[config.propertyName]) * factor;
+        }
+      });
+    }
+    else {
+      return array
+    }
   }
 }
